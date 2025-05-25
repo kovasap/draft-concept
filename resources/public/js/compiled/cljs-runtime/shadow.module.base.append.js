619fa03d6f8d55609b6a313f761f305e83131b8d
Kovas Palunas <kovas.palunas@gmail.com>
2025-05-25 13:54:38 -0700

    no compiled files in repo

--- resources/public/js/compiled/cljs-runtime/shadow.module.base.append.js
+++
@@ -1,4 +0,0 @@
-
-shadow.cljs.devtools.client.env.module_loaded('base');
-
-try { app.interface$.core.init(); } catch (e) { console.error("An error occurred when calling (app.interface.core/init)"); throw(e); }
